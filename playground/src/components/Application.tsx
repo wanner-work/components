a5d2fe8da64f226a36a3cbbd211ce65607c03a6d
jwanner83 <jonas@wanner.com>
2024-05-10 09:49:37 +0800

    feat: add the Box to the playground

--- playground/src/components/Application.tsx
+++ playground/src/components/Application.tsx
@@ -1,12 +1,17 @@
 import Image, { ImageProvider } from '@wanner.work/image'
+import Box from '@wanner.work/box'
 
 export default function Application() {
   return (
     <main>
       <div className="h-screen w-screen bg-black flex items-center justify-center gap-16">
-        <Image src="https://source.unsplash.com/random" alt="Random image" height={500} width={500}/>
+        <Box width="medium">
+          <Image src="https://source.unsplash.com/random" alt="Random image" fill />
+        </Box>
         <ImageProvider loader={<div className="text-white absolute h-full w-full">Loading...</div>}>
-          <Image src="https://source.unsplash.com/random" alt="Random image" height={500} width={500}/>
+          <Box width="medium">
+            <Image src="https://source.unsplash.com/random" alt="Random image" fill />
+          </Box>
         </ImageProvider>
       </div>
     </main>
